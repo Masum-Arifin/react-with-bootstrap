b970d5f2123a6addef9e1681e09edc1b39ae4196
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-02 01:42:38 +0600

    new code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { Button } from "react-bootstrap";
-import "./App.css";
-import CardGroup2 from "./components/CardGroup2/CardGroup2";
+// import { Button } from "react-bootstrap";
+// import "./App.css";
+// import CardGroup2 from "./components/CardGroup2/CardGroup2";
 
-function App() {
-  return (
+// function App() {
+//   return (
    
-      <div>
-        <h1>Using React Bootstrap</h1>
-        <Button variant="success">My Button</Button>
-        <CardGroup2></CardGroup2>
-      </div>
+//       <div>
+//         <h1>Using React Bootstrap</h1>
+//         <Button variant="success">My Button</Button>
+//         <CardGroup2></CardGroup2>
+//       </div>
     
-  );
-}
+//   );
+// }
 
-export default App;
+// export default App;
