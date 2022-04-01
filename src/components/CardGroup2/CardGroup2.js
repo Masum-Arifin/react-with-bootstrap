d8f6e20b9741db1166ed0dde12ad0ce014f1bcde
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-02 01:44:26 +0600

    new code

--- src/components/CardGroup2/CardGroup2.js
+++ src/components/CardGroup2/CardGroup2.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { CardGroup } from 'react-bootstrap';
-import Card2 from '../Card2/Card2';
+// import React from 'react';
+// import { CardGroup } from 'react-bootstrap';
+// import Card2 from '../Card2/Card2';
 
-const CardGroup2 = () => {
-    const products = [
-        { id: 1, name: "Laptop Pro", price: 1250000 },
-        { id: 2, name: "Laptop Pro max", price: 1250000 },
-        { id: 3, name: "Laptop Alta Pro", price: 1250000 },
-      ];
-    return (
-        <div>
-            <CardGroup>
-            {products.map((product) => (
-        <Card2 key={product.id} product={product}></Card2>
-      ))}
-            </CardGroup>
-        </div>
-    );
-};
+// const CardGroup2 = () => {
+//     const products = [
+//         { id: 1, name: "Laptop Pro", price: 1250000 },
+//         { id: 2, name: "Laptop Pro max", price: 1250000 },
+//         { id: 3, name: "Laptop Alta Pro", price: 1250000 },
+//       ];
+//     return (
+//         <div>
+//             <CardGroup>
+//             {products.map((product) => (
+//         <Card2 key={product.id} product={product}></Card2>
+//       ))}
+//             </CardGroup>
+//         </div>
+//     );
+// };
 
-export default CardGroup2;
+// export default CardGroup2;
